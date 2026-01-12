f0945ed0e9946cb4c0fd8c668b3c0afad3cdc938
genggui001 <genggui001@gmail.com>
2026-01-12 13:38:28 +0800

    add

--- toolkits/distributed_checkpoints_convertor/test_c.sh
+++ toolkits/distributed_checkpoints_convertor/test_c.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -xe
 
-bash scripts/qwen3/run_8xH20_my.sh \
-A3B \
-/mnt/nas/home/genggui/.cache/huggingface/hub/models--Qwen--Qwen3-30B-A3B-Thinking-2507/snapshots/144afc2f379b542fdd4e85a1fcd5e1f79112d95d \
-/mnt/nas/home/genggui/pretrain_weights/nlp/Qwen3-30B-A3B-Thinking-2507-to-mcore-mtp  \
+bash scripts/qwen3/run_A22B_16xH20_my.sh \
+A22B \
+/mnt/nas/home/genggui/.cache/huggingface/hub/models--Qwen--Qwen3-235B-A22B-Thinking-2507/snapshots/6cbffae6d8e28b986a6b17bd36f42f9fa0f1f0a5 \
+/mnt/nas/home/genggui/pretrain_weights/nlp/Qwen3-235B-A22B-Thinking-2507-to-mcore-mtp  \
 false \
 true \
 bf16 2>&1 | tee to-mcore.log
